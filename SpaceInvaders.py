f69aa021b7f886f45876c469350b77cd367b992c
Stephen <Blaney.stephen9@gmail.com>
2019-04-14 20:55:49 +0100

    Created our one enemy, enemy created main game loop
    Enemy movement and boundary control for the enemy also created

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -35,26 +35,58 @@ player.setheading(90)  # Now, initially our triangle is positioned to the right 
 # Step 4: Player controls
 player_speed = 15 # variable that contains our chractar speed for now
 
-# The following are two function that will move our player left and right respectively.
-def move_left():
+# Step 5: Create the enemy
+enemy = turtle.Turtle()  # Another turtle bbject created
+enemy.color("red")  # Set enemy color to red
+enemy.shape("circle") # Set the enemy shape to circle
+enemy.penup()  # We don't want anymore drawing in the background.
+enemy.speed(0)  # Fast as possible
+enemy.setposition(-200, 250)  # This will place our enemy somewhere on the top left of the screen
+
+enemy_speed = 2
+
+
+def move_left():  # The following are two function that will move our player left and right respectively.
     x = player.xcor()  # this is 0 when the game starts
     x -= player_speed  # subtracts the players speed and assigns it to x. X = 15
     if x < -280:  # If our player goes beyoud -280 pixels (our border on the left) it will be set back to - 280
         x = - 280
     player.setx(x)  # get current x subtract the speed and change the players location to the new x
 
-def move_right(): #Same method except we are adding seeing how we are moving to the right
+
+def move_right():  # Same method except we are adding seeing how we are moving to the right
     x = player.xcor()
     x += player_speed
     if x > 280:
         x = 280
     player.setx(x)
 
-
 # Create keyboard bindings
-turtle.listen()  # listen for keyboard actions
-turtle.onkey(move_left, "Left")  # when key is pressed move to the left
-turtle.onkey(move_right, "Right")  # when key is pressed move to the right.
+
+
+wn.listen()  # listen for keyboard actions
+wn.onkey(move_left, "Left")  # when key is pressed move to the left
+wn.onkey(move_right, "Right")  # when key is pressed move to the right.
+
+# Step 6: Main game loop
+while True: #when the game runs meaning forever
+    #  Move the enemy similar to our player from earlier
+    x = enemy.xcor()
+    x += enemy_speed
+    enemy.setx(x)
+
+    #boundary checking for enemy
+    if enemy.xcor() > 280:
+        enemy_speed *= -1 #Basically when our enemy goes over the boundary it needs to turn around and go back this is by we mutiple by postive 1 so it goes the other direction
+        y = enemy.ycor() #get current y cor
+        y -= 40 # bring down our enimy 40 pixels
+        enemy.sety(y)
+
+    if enemy.xcor() < -280:  # Same principle applies here
+        enemy_speed *= -1
+        y = enemy.ycor()
+        y -= 40
+        enemy.sety(y)
 
 
 
@@ -70,4 +102,5 @@ turtle.onkey(move_right, "Right")  # when key is pressed move to the right.
 
 
 
-turtle.done() # This command tells python that we are finnish using turle commands this effeictvely means that we have to reimport it again if we want to use the turtle module
+# This command tells python that we are finnish using turle commands this effeictvely means that we have to reimport it again if we want to use the turtle module
+wn.mainloop()
